ddba8b2b63ac6079348bc4fe47d8147cffe420fe
Danish Anwar <36497304+danish-anwar-butt@users.noreply.github.com>
2024-12-14 22:03:25 +0500

    main application project in eclipse Luna to test library
    thanks for sharing : http://codeintherightway.blogspot.com/2015/06/freertos-on-arduino-uno-using-eclipse.html

---
+++ rtostest/Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/danish/avr_ws/avrrtoslib/Release/libavrrtoslib.a
+
+LIBS :=
+
